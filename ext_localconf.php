b5468ec92a552784da5aa671d63452d7474e11c8
Christian Welzel <c.welzel@schech.net>
2020-06-15 10:23:55 +0200

    Use new configuration API.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,10 @@
 <?php
 defined('TYPO3_MODE') or die('Access denied.');
 
-$configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['ameos_tarteaucitron']);
+$configuration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+    \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
+)->get('ameos_tarteaucitron');
+
 
 if($configuration['xclass_youtube'] == '1'){
 	$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Resource\Rendering\YouTubeRenderer::class] = array(
